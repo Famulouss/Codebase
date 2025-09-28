d5c128918e6b58efda3345b13d7949251365b80f
Famulouss <kompatscherjulius@gmail.com>
2025-09-28 18:09:06 +0200

    Neue Filtergewichtung für Yawbeschleunigung (We). Plotten der nach Norm gefilterten Signale. Berechnen der gewichteten RMS Werte der Beschleunigungen x, y und yaw.

--- Metric_analysis_EDGAR.py
+++ Metric_analysis_EDGAR.py
@@ -78,7 +78,6 @@ def design_wd_filter(fs):
     fs: Abtastrate in Hz
     returns: b, a (arrays)
     """
-    T = 1.0 / fs
     omega1 = 2 * np.pi * 0.4    # f1 = 0,4 Hz
     omega2 = 2 * np.pi * 100.0  # f2 = 100 Hz
     omega3 = 2 * np.pi * 2.0    # f3 = 2 Hz
@@ -87,7 +86,7 @@ def design_wd_filter(fs):
 
     # Konstruktion der Teilfilter (analog)
     num_h, den_h = highpass_norm(omega1)    # Hochpassfilter
-    num_l, den_l = lowpass_filter(omega2)   # Tiefpassfilter
+    num_l, den_l = lowpass_norm(omega2)   # Tiefpassfilter
     num_t, den_t = acc_vel_transition_filter(omega3, omega4, q4)    # Acceleration-velocity transition Filter
     
     # Gesamtnumerator/-denominator durch Polynom-Multiplikation (Faltung der Koeffizienten)
@@ -99,6 +98,34 @@ def design_wd_filter(fs):
     # Diskretisierung/Umwandlung in digitales Filter (bilineare Transformation)
     b, a = signal.bilinear(num_total, den_total, fs)
     
+    return b, a
+
+def design_we_filter(fs):
+    """
+    Entwirft digitale Filterkoeffizienten für We-Gewichtung gemäß ISO 2631 (Annäherung).
+    fs: Abtastrate in Hz
+    returns: b, a (arrays)
+    """
+    omega1 = 2 * np.pi * 0.4    # f1 = 0,4 Hz
+    omega2 = 2 * np.pi * 100.0  # f2 = 100 Hz
+    omega3 = 2 * np.pi * 1.0    # f3 = 1 Hz
+    omega4 = 2 * np.pi * 1.0    # f4 = 1 Hz
+    q4 = 0.63
+
+    # Konstruktion der Teilfilter (analog)
+    num_h, den_h = highpass_norm(omega1)    # Hochpassfilter
+    num_l, den_l = lowpass_norm(omega2)   # Tiefpassfilter
+    num_t, den_t = acc_vel_transition_filter(omega3, omega4, q4)    # Acceleration-velocity transition Filter
+    
+    # Gesamtnumerator/-denominator durch Polynom-Multiplikation (Faltung der Koeffizienten)
+    num_temp = np.convolve(num_h, num_l)
+    num_total = np.convolve(num_temp, num_t)
+    den_temp = np.convolve(den_h, den_l)
+    den_total = np.convolve(den_temp, den_t)
+
+    # Diskretisierung/Umwandlung in digitales Filter (bilineare Transformation)
+    b, a = signal.bilinear(num_total, den_total, fs)
+
     return b, a
 
 # -------------------
@@ -131,7 +158,7 @@ print(f'Column names: {df.columns}')
 # -------------------
 # Filter anwenden
 # -------------------
-fs = 10.0      # Abtastrate [Hz]
+fs = 40.0      # Abtastrate [Hz] (25ms)
 cutoff = 2.0   # Grenzfrequenz [Hz]
 
 df["acc_x_filt"] = lowpass_filter(df["acc_x"], cutoff, fs)
@@ -202,5 +229,52 @@ sum_x = integral_abs(df['time_rel'], df['acc_x'])
 sum_y = integral_abs(df['time_rel'], df['acc_y'])
 print(f"Integral x-Beschleunigung: {sum_x}\nIntegral y-Beschleunigung: {sum_y}")
 
-
+# -------------------
 # ISO 2631 Metrik (RMS der frequenzgewichteten Beschleunigung)
+# -------------------
+b, a = design_wd_filter(fs)     # Gewichtung ist laut Norm für die x- und y-Beschleunigung diesselbe
+df['acc_x_normfilt'] = filtfilt(b, a, df['acc_x'])
+df['acc_y_normfilt'] = filtfilt(b, a, df['acc_y'])
+b, a = design_we_filter(fs)
+df['yaw_acc_normfilt'] = filtfilt(b, a, df['yaw_acc'])
+df['yaw_acc_normfilt'] *= 0.2   # k-Faktor nach Norm [m/rad]
+
+# Integral für den frequenzgewichteten RMS (∫(aw^2)dt)
+I_x = np.trapezoid(df['acc_x_normfilt']**2, df['time_rel'])  
+I_y = np.trapezoid(df['acc_y_normfilt']**2, df['time_rel'])
+I_yaw = np.trapezoid(df['yaw_acc_normfilt']**2, df['time_rel'])
+# weighted r.m.s (sqrt(1/T*I))
+awx = np.sqrt(1/df['time_rel'].iloc[-1]*I_x)   
+awy = np.sqrt(1/df['time_rel'].iloc[-1]*I_y)
+awyaw = np.sqrt(1/df['time_rel'].iloc[-1]*I_yaw)
+print(f"Weighted RMS x-acceleration: {awx} m/s²")
+print(f"Weighted RMS y-acceleration: {awy} m/s²")
+print(f"Weighted RMS yaw-acceleration: {awyaw} rad/s²2")
+
+# Gefilterte Beschleunigungsgrafiken
+fig, axes = plt.subplots(3, 1, figsize=(12, 12), sharex=True)
+
+axes[0].plot(df['time_rel'], df['acc_x'], label='Raw')
+axes[0].plot(df['time_rel'], df['acc_x_normfilt'], label='Filtered', linewidth=2)
+axes[0].set_ylabel('Acceleration [m/s²]')
+axes[0].set_title('Norm-filtered x-acceleration')
+axes[0].grid()
+axes[0].legend()
+
+axes[1].plot(df['time_rel'], df['acc_y'], label='Raw')
+axes[1].plot(df['time_rel'], df['acc_y_normfilt'], label='Filtered', linewidth=2)
+axes[1].set_ylabel('Acceleration [m/s²]')
+axes[1].set_title('Norm-filtered y-acceleration')
+axes[1].grid()
+axes[1].legend()
+
+axes[2].plot(df['time_rel'], df['yaw_acc'], label='Raw')
+axes[2].plot(df['time_rel'], df['yaw_acc_normfilt'], label='Filtered', linewidth=2)
+axes[2].set_ylabel('Acceleration [rad/s²]')
+axes[2].set_xlabel('Time [s]')
+axes[2].set_title('Norm-filtered yaw-acceleration')
+axes[2].grid()
+axes[2].legend()
+
+plt.tight_layout()
+plt.show()
